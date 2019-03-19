7900218e89b977024aa640f57b1a1765fd6f884b
oliverwood93 <oliverwood1993@hotmail.co.uk>
2019-03-19 15:51:54 +0000

    alter formatting style of dbconnection

--- ssc/dbconnection.py
+++ ssc/dbconnection.py
@@ -2,7 +2,8 @@ import psycopg2
 from ssc.dbconfig import user, password
 
 connection = psycopg2.connect(
-            user = user,
-            password = password,
-            database = "ssc")
-        cursor = connection.cursor()
+    user = user,
+    password = password,
+    database = "ssc")
+
+cursor = connection.cursor()
